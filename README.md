e7e0803491061672b586dff789c13c0fe0381e51
diliaur <diliaur@hawaii.edu>
2018-08-31 11:42:46 -1000

    first commit...

---
+++ README.md
@@ -0,0 +1,1 @@
+Building a small wordpress plugin to port data in from a google sheet, display it nicely, and allow filtration by column. Test case is the scholarships page for my place of work. Eventually the plugin will be generalized (how this will look conceptually will be addressed at some point in the future).
---
+++ scholarships.js
@@ -0,0 +1,28 @@
+console.log("connected!"); // runs before any of the doc is loaded.
+
+/* Link to the spreadsheet */
+// https://docs.google.com/spreadsheets/d/13JTS8DX4z5BZ6bTwgkmssxcERodv6Tl5OKaj_EJ2qIA/edit?usp=sharing
+
+/* 
+* processing data from google sheet via Tabletop
+*/
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/13JTS8DX4z5BZ6bTwgkmssxcERodv6Tl5OKaj_EJ2qIA/edit?usp=sharing';
+
+function init() {
+	Tabletop.init( { key: publicSpreadsheetUrl,
+	                 callback: showInfo,
+	                 simpleSheet: true, 
+	             	 prettyColumnNames: false} )
+}
+
+function showInfo(data, tabletop) {
+	console.log('Successfully processed!')
+
+	// data is an array of objects
+	console.log(data);
+	data.forEach( scholarship => {
+		
+	});
+}
+
+window.addEventListener('DOMContentLoaded', init)

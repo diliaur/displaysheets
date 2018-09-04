75a0b2a3eb2a757a881dedb511a61f55c866c481
diliaur <diliaur@hawaii.edu>
2018-09-04 11:28:18 -1000

    enter data into template

--- scholarships.js
+++ scholarships.js
@@ -17,11 +17,45 @@ function init() {
 
 function showInfo(data, tabletop) {
 	console.log('Successfully processed!')
-
 	// data is an array of objects
-	console.log(data);
+	// console.log(data);
+
+	var wrapper = $('.award-wrapper');
+	var award = $('.award',wrapper).clone();
+	wrapper.empty(); // make room for new appends
+
 	data.forEach( scholarship => {
-		// use a template
+		// copy the original EMPTY award div) as a temp
+		var tempAward = award.clone();
+		
+		// -----------------------
+		// fill out the temp award
+		// -----------------------
+		// about the award
+		$('.award-header',tempAward).append(scholarship.name);
+		$('.fund-source',tempAward).append('Fund source: ' + scholarship.fundsource);
+		$('.length',tempAward).append('Length: ' + scholarship.length);
+		$('.type',tempAward).append('Funding type: ' + scholarship.fundingtype);
+		$('.place-of-study',tempAward).append('Place of study: ' + scholarship.placeofstudy);
+		$('.sea-country-focus',tempAward).append('SEA country focus: ' + scholarship.seacountryfocus);
+		$('.discipline-focus',tempAward).append('Discipline focus: ' + scholarship.disciplinefocus);
+		// eligibility info
+		$('.class-level',tempAward).append('Class level: ' + scholarship.gradelevel);
+		$('.citizenship',tempAward).append('Citizenship: ' + scholarship.citizenship);
+		// more info
+		$('.website',tempAward).append('<a href=\'' + scholarship.url + '\'>Website</a>');
+		$('.contact-point',tempAward).append('Contact point: ' + scholarship.contactname + ' (' + scholarship.contactemail + ')');
+		$('.description',tempAward).append('Description:<br/>' + scholarship.description);
+
+		// $('.',tempAward).append(': ' + scholarship.);
+
+		// -----------------------
+		// end award fill out
+		// -----------------------
+
+		// append it to the wrapper
+		wrapper.append(tempAward);
+
 		});
 
 }

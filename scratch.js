9dd1daf8037aeba3f15769ae4660acdabce903a2
diliaur <diliaur@hawaii.edu>
2018-09-10 11:23:56 -1000

    convert to tables using List.js templating

--- scratch.js
+++ scratch.js
@@ -20,8 +20,10 @@ function showInfo(data, tabletop) {
 	/* do the list.js stuff ... in here I guess */
 
 	var options = {
-		valueNames: [ 'name', 'length' ],
-		item: '<div class="award"><div class="name"></div><div class="length"></div></div>'
+		valueNames: [ 'name', {name: 'url', attr: 'href'}, 'fundsource', 'fundingtype', 'length', 
+					  'placeofstudy', 'seacountryfocus', 'disciplinefocus',
+					  'gradelevel', 'citizenship', 'contactname', 'contactemail'],
+		item: '<tr><td class="name">Name</td><td><a class="url" target="_blank">More Info</a></td><td class="fundsource">Funding Source</td><td class="fundingtype">Funding Type</td><td class="length">Length</td><td class="placeofstudy">Place of Study</td><td class="seacountryfocus">SEA Country Focus</td><td class="disciplinefocus">Discipline Focus</td><td class="gradelevel">Class Level</td><td class="citizenship">Citizenship Requirement</td><td class="contactname">Contact Name</td><td class="contactemail">Contact Email</td></tr>'
 	};
 
 	var userList = new List('award-wrapper', options, data);

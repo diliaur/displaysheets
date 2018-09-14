d8efc7668153de6b5c10a51d255a06249afe3e7d
diliaur <diliaur@hawaii.edu>
2018-09-14 12:37:26 -1000

    add filter when citizenship box is checked

--- scratch.js
+++ scratch.js
@@ -26,7 +26,17 @@ function showInfo(data, tabletop) {
 		item: '<tr><td class="name">Name</td><td><a class="url" target="_blank">More Info</a></td><td class="fundsource">Funding Source</td><td class="fundingtype">Funding Type</td><td class="length">Length</td><td class="placeofstudy">Place of Study</td><td class="seacountryfocus">SEA Country Focus</td><td class="disciplinefocus">Discipline Focus</td><td class="gradelevel">Class Level</td><td class="citizenship">Citizenship Requirement</td><td class="contactname">Contact Name</td><td class="contactemail">Contact Email</td></tr>'
 	};
 
-	var userList = new List('award-wrapper', options, data);
+	var awardList = new List('award-wrapper', options, data);
+
+	let box = document.getElementById('check-citizenship');
+	
+	box.addEventListener('click',function(){
+		if (box.checked) {
+			awardList.search("us",['citizenship']);
+		} else {
+			awardList.search("");
+		}
+	});
 
 }
 
